6c5e1b5319f5220b34c267d902f8ca7c015ac1a1
manikantan2050 <manikantan2050@yahoo.com>
2026-04-10 21:41:43 -0400

    [Jinjaneering] Deploy test joiner_lookup_m_joiner_lkp_test.sql from project PROJECT10

---
+++ tests/joiner_lookup_m_joiner_lkp_test.sql
@@ -0,0 +1,10 @@
+-- Jinjaneering validation test for Joiner + Lookup Enrichment
+-- Pattern: joiner_lookup
+-- Tests: Fail if model has zero rows (empty output is invalid)
+
+select
+    'joiner_lookup_m_joiner_lkp' as model_name,
+    'row_count_check' as test_type,
+    count(*) as total_rows
+from {{ ref('joiner_lookup_m_joiner_lkp') }}
+having count(*) = 0
